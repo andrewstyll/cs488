8ee9399cd78cc7ab2b8aab9f8aa4584f36480539
andrewstyll <andrewstyll@gmail.com>
2019-09-21 02:33:40 -0400

    completed A0

---
+++ A4/Primitive.cpp
@@ -0,0 +1,23 @@
+// Spring 2019
+
+#include "Primitive.hpp"
+
+Primitive::~Primitive()
+{
+}
+
+Sphere::~Sphere()
+{
+}
+
+Cube::~Cube()
+{
+}
+
+NonhierSphere::~NonhierSphere()
+{
+}
+
+NonhierBox::~NonhierBox()
+{
+}
